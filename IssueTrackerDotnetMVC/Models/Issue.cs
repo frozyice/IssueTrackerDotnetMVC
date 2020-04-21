22961b3dbf895bc454bf8859c6c5c40f5a3a65b9
Karmo <Karmo.Kiima@Eesti.ee>
2020-04-21 16:08:00 +0300

    Add Issue model

---
+++ IssueTrackerDotnetMVC/Models/Issue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IssueTrackerDotnetMVC.Models
+{
+    public class Issue
+    {
+        public Guid IssueId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public Issue()
+        {
+            IssueId = Guid.NewGuid();
+        }
+    }
+}
